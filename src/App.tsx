99f80c8c6d9d765da4b2e4f08e442a1b284ed6ef
IsaiasArza <arza.isaias@gmail.com>
2023-10-18 11:28:40 -0300

    removing spinner

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,15 @@
  * @format
  */
 
-import React, { useState } from 'react';
+import React from 'react';
 
-import { Dialog, ThemeProvider, createTheme } from '@rneui/themed';
+import { ThemeProvider, createTheme } from '@rneui/themed';
 import { Provider } from 'react-redux';
 import store from './Core/Data/Redux/Store';
 import { MainRoutesView } from './Core/Presentation/MainRoutesView';
 
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
-import axios from 'axios';
 
 const theme = createTheme({
   lightColors: {
@@ -32,29 +31,9 @@ const theme = createTheme({
 
 function App(): React.JSX.Element {
   dayjs.extend(utc);
-  const [spinner, setSpinner] = useState(false);
-
-  axios.interceptors.request.use(function (config) {
-    setSpinner(true);
-    return config;
-  });
-
-  axios.interceptors.response.use(
-    function (response) {
-      setSpinner(false);
-      return response;
-    },
-    function (error) {
-      setSpinner(false);
-      return Promise.reject(error);
-    },
-  );
   return (
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <Dialog isVisible={spinner}>
-          <Dialog.Loading />
-        </Dialog>
         <MainRoutesView></MainRoutesView>
       </Provider>
     </ThemeProvider>
